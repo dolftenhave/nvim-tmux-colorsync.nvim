2fdaac10ec4cbb8671091bbd2e23f707d7d98f16
dolftenhave <dolftenhave@gmail.com>
2025-08-17 17:09:49 +1200

    Updated the he hex field to allow for nill values

--- lua/color.lua
+++ lua/color.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 ---@class Color
----@field hex string The hex value of the color.
----@field destination_name string The name of the color that will be updated in the .conf file
+---@field hex string? The hex value of the color.
+---@field destination_name string The name of the color that will be updated in the colors.conf file
 ---@field hl_group_name string The the highlight group name of the color in vim.
 ---@field event string The name of the event that when triggered, updates the color in tmux.
 local Color = {}
