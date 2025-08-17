ded618080fb2f804bb142b8718c9d8ead27022fd
dolftenhave <dolftenhave@gmail.com>
2025-08-17 14:23:38 +1200

    Added a color type that holds all the values needed to update a color

---
+++ lua/colors.lua
@@ -0,0 +1,10 @@
+---@class Colors
+local Colors = {}
+
+---@class Color
+---@field hex string The hex value of the color.
+---@field source_name string The name of the color source.
+---@field event string The name of the event that when triggered, updates the color in tmux.
+local Color = {}
+
+return Color
