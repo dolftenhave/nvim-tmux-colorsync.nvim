ac8bc9d2146c0df21b5ce2009ca3079fbbe2c6f4
dolftenhave <dolftenhave@gmail.com>
2025-08-17 16:47:07 +1200

    Renamed the Colors modle to M to conform with convention

--- lua/color.lua
+++ lua/color.lua
@@ -1,10 +1,10 @@
----@class Colors
-local Colors = {}
+local M = {}
 
 ---@class Color
 ---@field hex string The hex value of the color.
----@field source_name string The name of the color source.
+---@field destination_name string The name of the color that will be updated in the .conf file
+---@field hl_group_name string The the highlight group name of the color in vim.
 ---@field event string The name of the event that when triggered, updates the color in tmux.
 local Color = {}
 
-return Color
+return M
