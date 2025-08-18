94c6ce6e298896aa7abe6e5be2b0a8b372f72f65
dolftenhave <dolftenhave@gmail.com>
2025-08-18 20:14:44 +1200

    Added a line function to TmuxColor to concatinati the name and a hex value for easy printing.

--- lua/color.lua
+++ lua/color.lua
@@ -1,6 +1,10 @@
 ---@class Colors
 local M = {}
 
+---@alias key
+---| '"hex"' # The potentially modifiable color value.
+---| '"default"' # The default color value.
+
 ---@class Color
 ---@field hex string? The hex value of the color.
 ---@field destination_name string The name of the color that will be updated in the colors.conf file
@@ -10,7 +14,15 @@ M.Color = {}
 ---@field hex string The hex value of the color.
 ---@field default string The value of the color when it was first read.
 ---@field name string The name of the color that will be updated in the colors.conf file.
-M.TmuxColor = {}
+M.TmuxColor = {
+
+	---@param key key The key of the hex value.
+	---@return string line A formatted line that can be written to the tmux color file.
+	line = function(self, key)
+		local line = self.name .. '="' .. self[key] .. '"'
+		return line
+	end,
+}
 
 ---@class VimColor
 ---@field name string The index of the TmuxColor in the colors array.
